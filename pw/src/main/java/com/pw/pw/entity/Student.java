95f874dcc4bf0d49a9d3e946b3afbeb0851cefa0
vaibhavkhot1709 <113294171+vaibhavkhot1709@users.noreply.github.com>
2025-02-24 18:24:39 +0530

    Update Student.java

--- pw/src/main/java/com/pw/pw/entity/Student.java
+++ pw/src/main/java/com/pw/pw/entity/Student.java
@@ -3,6 +3,7 @@ package com.pw.pw.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,9 +35,13 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int stdId;
 	
+	 @Column(nullable = false)
+	@NotNull(message = "First name should not be NULL")
 	private String firstName;
+	 @NotNull(message = "Last name should not be NULL")
 	private String lastName;
 	private String email;
+	@NotNull(message = "Contact should not be NULL")
 	private String contct;
 	
 	@OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
