431ed5ecc9b2ba5255ef50f5c96086be29b8d9ab
vaibhavkhot1709 <113294171+vaibhavkhot1709@users.noreply.github.com>
2025-02-24 18:22:43 +0530

    Update StdController.java

--- pw/src/main/java/com/pw/pw/controller/StdController.java
+++ pw/src/main/java/com/pw/pw/controller/StdController.java
@@ -1,6 +1,10 @@
 package com.pw.pw.controller;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,6 +25,7 @@ import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("/pwstd")
+
 public class StdController {
 
 	
@@ -28,45 +33,69 @@ public class StdController {
 	StdServiceImpl serviceImpl;
 	
 	@PostMapping("/student")
-	public ResponseEntity<Student> saveStudent(@RequestBody Student Student){
-		
-		Student saveStudent= serviceImpl.saveStudent(Student);
-		return new ResponseEntity<>(saveStudent, HttpStatus.CREATED);
+	@Transactional
+	public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+	
+//		Student saveStudent= serviceImpl.saveStudent(student);
+		return new ResponseEntity<>(serviceImpl.saveStudent(student), HttpStatus.CREATED);
 	}
 	
 	@GetMapping("/student/{std_id}")
-	public ResponseEntity<Student> getStudentById(@PathVariable("std_id") int stdId){
+	@Transactional
+	public ResponseEntity<Object> getStudentById(@PathVariable("std_id") int stdId){
 
-		Student saveStudent = serviceImpl.getStudentById(stdId);
-		return new ResponseEntity<>(saveStudent, HttpStatus.OK);
+
+		
+		Student getStudent = serviceImpl.getStudentById(stdId);
+		if(getStudent==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("message", "Student with ID " + stdId + " is not found send valid Student ID"));
+		}
+		return new ResponseEntity<>(getStudent, HttpStatus.OK);
 	}
 	
 	@GetMapping("/students")
-	public ResponseEntity<java.util.List<Student>> getAllStudents(){
+	public ResponseEntity<List<Student>> getAllStudents(){
 		
-		List<Student> saveStudent = serviceImpl.getAllStudent();
-		return new ResponseEntity<>(saveStudent, HttpStatus.OK);
+//		List<Student> saveStudent = serviceImpl.getAllStudent();
+//		return new ResponseEntity<>(saveStudent, HttpStatus.OK);
+		return ResponseEntity.ok(serviceImpl.getAllStudent());
 	}
 	
 	@PutMapping("/student/{std_id}")
 	@Transactional
-	public ResponseEntity<Student> updateStudentById(@PathVariable("std_id") int stdId, @RequestBody Student newStudent){
+	public ResponseEntity<Map<String, Object>> updateStudentById(@PathVariable("std_id") int stdId, @RequestBody Student newStudent){
 
+		 Map<String, Object> response = new HashMap();
+		 
+		 Student saveStudent = serviceImpl.getStudentById(stdId);
+			
+			if(saveStudent==null) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("message", "Student with ID " + stdId + " is not found send valid Student ID"));
+			}
+			
+		 
 		Student updatedStd=serviceImpl.updateStudentById(stdId, newStudent);
+		
+		response.put("message", "Student with id "+ stdId+" is updated");
+		response.put("update student", updatedStd);
 	
-		return new ResponseEntity<>(updatedStd, HttpStatus.OK);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 	
 	@DeleteMapping("/student/{std_id}")
-	public ResponseEntity deleteStudentById(@PathVariable("std_id") int stdI){
+	public ResponseEntity deleteStudentById(@PathVariable("std_id") int stdId){
+		Student saveStudent = serviceImpl.getStudentById(stdId);
 		
-		serviceImpl.deletStundetById(stdI);
+		if(saveStudent==null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("message", "Student with ID " + stdId + " is not found send valid Student ID"));
+		}
+		serviceImpl.deletStundetById(stdId);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 	@DeleteMapping("/students")
 	public ResponseEntity deleteAllStudets(){
-		
+				
 		serviceImpl.deleteAllStudent();
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
